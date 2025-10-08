b1452fe9d61fe541c9d494a2b27e97fa745ca4ae
Mohamed <mohamed9898magdy@gmail.com>
2025-10-09 00:49:04 +0300

    Refactor repositories by generating Generic Interface and implementation for common methods.

---
+++ Data Access Layer/Repositories/Classes/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using Data_Access_Layer.Data.Contexts;
+using Data_Access_Layer.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Repositories.Classes
+{
+    internal class GenericRepository<TEntity> : IgenericRepository<TEntity> where TEntity : class, new()
+    {
+        private readonly GymSystemDBContext context;
+
+        public GenericRepository(GymSystemDBContext context)
+        {
+            this.context = context;
+        }
+
+        public int Add(TEntity entity)
+        {
+            context.Set<TEntity>().Add(entity);
+            return context.SaveChanges();
+
+        }
+
+        public int Delete(int id)
+        {
+            var entity = context.Set<TEntity>().Find(id);
+
+            if (entity is null)
+            {
+                return 0; 
+            }
+
+            context.Set<TEntity>().Remove(entity);
+            return context.SaveChanges();
+
+        }
+
+        public IEnumerable<TEntity> GetAll() => context.Set<TEntity>().AsNoTracking().ToList();
+
+
+        public TEntity? GetById(int id) => context.Set<TEntity>().Find(id);
+       
+
+        public int Update(TEntity entity)
+        {
+            var entry = context.Set<TEntity>().Find(entity);
+            if (entry is not null)
+                context.Set<TEntity>().Update(entity);
+                
+           return context.SaveChanges(); 
+
+        }
+    }
+}
--- Presentation Layer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Presentation_Layer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
