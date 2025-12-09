5d07fffca9d49e4a89c07b4669d3936e4424da38
Mohamed <mohamed9898magdy@gmail.com>
2025-12-09 10:00:00 +0200

    Trainer Service (Task)

---
+++ Business Logic Layer/Services/TranierService/Interface/ITrainerService.cs
@@ -0,0 +1,24 @@
+﻿using Business_Logic_Layer.ViewModels.TrainerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.Services.TranierService.Interface
+{
+    public interface ITrainerService
+    {
+        IEnumerable<TrainerViewModel> GetTrainers();
+
+        bool CreateTrainer(CreateTrainerViewModel createdTrainer);
+
+        TrainerViewModel? GetTrainerDetails(int TrainerId);
+
+        TrainerToUpdateViewModel? GetTrainerToUpdate(int TrainerId);
+
+        bool UpdateTrainer(int TrainerId,TrainerToUpdateViewModel UpdatedTrainer);
+
+        bool DeleteTrainer(int TrainerId);
+    }
+}
---
+++ Business Logic Layer/ViewModels/TrainerViewModels/TrainerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.ViewModels.TrainerViewModels
+{
+    public class TrainerViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Specialization { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+
+
+
+    }
+}
