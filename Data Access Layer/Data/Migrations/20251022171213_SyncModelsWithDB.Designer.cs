89f014454e7013fe26e3a79f49462536929ef1d3
Mohamed <mohamed9898magdy@gmail.com>
2025-12-09 10:00:00 +0200

    Layout - ViewStart

--- Data Access Layer/Data/Migrations/20251022171213_SyncModelsWithDB.Designer.cs
+++ Data Access Layer/Data/Migrations/20251022171213_SyncModelsWithDB.Designer.cs
@@ -4,6 +4,7 @@ using Data_Access_Layer.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data_Access_Layer.Data.Migrations
 {
     [DbContext(typeof(GymSystemDBContext))]
-    partial class GymSystemDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251022171213_SyncModelsWithDB")]
+    partial class SyncModelsWithDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
