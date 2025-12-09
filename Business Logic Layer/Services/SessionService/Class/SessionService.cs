7d86fd1340acefe2dd0d17bf5173b7a1d8ae56d1
Mohamed <mohamed9898magdy@gmail.com>
2025-12-09 10:00:00 +0200

    Create and Update Session Services

--- Business Logic Layer/Services/SessionService/Class/SessionService.cs
+++ Business Logic Layer/Services/SessionService/Class/SessionService.cs
@@ -3,6 +3,7 @@ using Business_Logic_Layer.Services.SessionService.Interface;
 using Business_Logic_Layer.ViewModels.SessionViewModels;
 using Data_Access_Layer.Entities;
 using Data_Access_Layer.Unit_Of_Work.Interface;
+using GymManagementSystemBLL.ViewModels.SessionViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,24 @@ namespace Business_Logic_Layer.Services.SessionService.Class
         {
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
+        }
+
+        public bool CreateSession(CreateSessionViewModel createdSessionViewModel)
+        {
+            //check if Trainer exists
+           if(!IsTrainerExists(createdSessionViewModel.TrainerId)) return false;
+            // check if Category exists
+            var category = unitOfWork.GetRepository<Category>().GetById(createdSessionViewModel.CategoryId);
+          if(!IsCategoryExists(createdSessionViewModel.CategoryId)) return false;
+            // validate dates
+          
+          if(!AreDatesValid(createdSessionViewModel.StartDate,createdSessionViewModel.EndDate)) return false;
+
+            var mappedSession = mapper.Map<CreateSessionViewModel,Session>(createdSessionViewModel);
+            
+            
+            unitOfWork.GetRepository<Session>().Add(mappedSession);
+                    return unitOfWork.SaveChanges() > 0 ;
         }
 
         public IEnumerable<SessionViewModel> GetAllSessions()
@@ -51,5 +70,78 @@ namespace Business_Logic_Layer.Services.SessionService.Class
             
             return MappedSession;
         }
+
+        public bool UpdateSession(int SessionId, UpdateSessionViewModel updatedSessionViewModel)
+        {
+           var session = unitOfWork.GetRepository<Session>().GetById(SessionId);
+            // check if session is available for updating
+            // has no bookings and dates are in future
+            // and check if He changed id through request
+            if (!IsSessionAvailableForUpdating(session!)) return false;
+            //check if Trainer exists
+            if (!IsTrainerExists(updatedSessionViewModel.TrainerId)) return false;
+            // validate dates
+            if (!AreDatesValid(updatedSessionViewModel.StartDate, updatedSessionViewModel.EndDate)) return false;
+          
+            var mappedSession = mapper.Map<UpdateSessionViewModel,Session>(updatedSessionViewModel,session!);
+          
+            session!.UpdatedAt = DateTime.Now;
+
+            unitOfWork.GetRepository<Session>().Update(session!);
+
+                    return unitOfWork.SaveChanges() > 0;
+        }
+
+        public UpdateSessionViewModel? GetSessionForUpdate(int SessionId)
+        {
+            var session = unitOfWork.GetRepository<Session>().GetById(SessionId);
+
+            if (!IsSessionAvailableForUpdating(session!)) return null;
+
+            var mappedSession = mapper.Map<Session,UpdateSessionViewModel>(session!);
+            return mappedSession;
+        }
+
+
+
+        #region Private Helper Methods
+
+        private bool IsSessionAvailableForUpdating(Session session)
+        {
+            if(session is null) return false;
+
+            if(unitOfWork.sessionRepository.GetCountOfBookings(session.Id) > 0) return false;
+            
+            if(session.StartTime <= DateTime.Now) return false;
+
+            if(session.EndTime <= DateTime.Now) return false;
+
+                    return true;    
+
+
+        }
+
+        private bool IsTrainerExists(int trainerId)
+        {
+           return unitOfWork.GetRepository<Trainer>().GetById(trainerId) is not null;
+
+        }
+
+        private bool IsCategoryExists(int categoryId)
+        {
+            return unitOfWork.GetRepository<Category>().GetById(categoryId) is not null;
+        }
+
+        private bool AreDatesValid(DateTime startDate, DateTime endDate)
+        {
+            return startDate < endDate;
+        }
+
+     
+
+
+
+        #endregion
+
     }
 }
