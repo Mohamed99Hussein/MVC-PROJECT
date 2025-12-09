28f1d4b0815e998e98469a72a48fb4a94170f6ff
Mohamed <mohamed9898magdy@gmail.com>
2025-12-09 10:00:00 +0200

    Attachment Service

---
+++ Business Logic Layer/Services/AttachmentService/IAttachmentService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.Services.AttachmentService
+{
+    public interface IAttachmentService
+    {
+        string? Upload(string folderName,IFormFile file);
+
+        bool Delete(string fileName, string folderName);
+
+
+    }
+}
