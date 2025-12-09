bc2ba92823f60c71afbd5695572b5fbc0527e702
Mohamed99Hussein <mohamedbaki1810@gmail.com>
2025-12-09 10:00:00 +0200

    999999999999999

--- Presentation Layer/Program.cs
+++ Presentation Layer/Program.cs
@@ -5,7 +5,7 @@ namespace Presentation_Layer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args).
         {
             .
             var builder = WebApplication.CreateBuilder(args);
